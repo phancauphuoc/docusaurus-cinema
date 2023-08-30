ccf827719104c26ee540089ae6cdb859b9d21285
PhanPhuoc <ldcc.phuocphan@lotte.vn>
2023-08-30 10:01:09 +0700

    first commit

---
+++ docusaurus.config.js
@@ -0,0 +1,148 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Welcome',
+  tagline: 'Lotte Cinema are cool',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-test-site.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'Lotte Cinema',
+        logo: {
+          alt: 'Lotte Cinema Logo',
+          src: 'img/logo1.svg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'User Manual',
+          },
+          { to: '/blog', label: 'Blog', position: 'left' },
+          // {
+          //   href: 'https://github.com/facebook/docusaurus',
+          //   label: 'GitHub',
+          //   position: 'right',
+          // },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'User Manual',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Contact',
+            items: [
+              {
+                label: 'Facebook',
+                href: 'https://www.facebook.com/lottecinema',
+              },
+              {
+                label: 'Web',
+                href: 'https://www.lottecinemavn.com',
+              },
+              {
+                label: 'Instagram',
+                href: 'https://www.instagram.com/lottecinema_vietnam',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'LDCC',
+                to: 'https://ldcc.vn/',
+              },
+            ],
+          },
+          {
+            title: 'About',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              // {
+              //   label: 'GitHub',
+              //   href: 'https://github.com/facebook/docusaurus',
+              // },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Produced by Lotte Data Communicationn`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
---
+++ src/components/HomepageFeatures/index.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css';
+// import {} from '../../../static/img/img_home.png'
+
+const FeatureList = [
+  {
+    title: 'Easy to Use',
+    // Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    img_cinema: 'https://res.cloudinary.com/phanphuoc/image/upload/v1693325739/ygbzlr72zybpjsgaonfw.png',
+    description: (
+      <>
+        Lotte Cinema is designed to provide a user-friendly and easily accessible experience.
+      </>
+    ),
+  },
+  {
+    title: 'Focus on What Matters',
+    // Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    img_cinema: 'https://res.cloudinary.com/phanphuoc/image/upload/v1693325838/q0nbgp1iw1k6vci8ne5d.png',
+    description: (
+      <>
+        This document allows you to focus on your documents, while we take care of the tasks.
+        Please proceed by moving your documents into the <code>docs</code> directory.
+      </>
+    ),
+  },
+  {
+    title: 'Powered by Dev',
+    // Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    img_cinema: 'https://res.cloudinary.com/phanphuoc/image/upload/v1693325837/xkusifbmdbwfizatr5gn.png',
+    description: (
+      <>
+        With many years of programming experience, our team is ready to provide 24/7 support.
+      </>
+    ),
+  },
+];
+
+function Feature({ Svg, title, description, img_cinema }) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        {/* <Svg className={styles.featureSvg} role="img" /> */}
+        <img src={img_cinema} className={styles.featureSvg} />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
